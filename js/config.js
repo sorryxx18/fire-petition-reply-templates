765c705e558a73f3c94c16b3373d9c889da7f434
黃雷夫 <sorryxx18@gmail.com>
2026-09-02 23:32:01 +0800

    Plug in Google OAuth Client ID for Sign-In With Google

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const GAS_URL = "https://script.google.com/macros/s/AKfycbzBxudRZ8OBDPij8onjwRDLkhrDc22sKjzFciOHGVpOCR29K_mYyOHg6w95D73qWR9f9A/exec";
 const ADMIN_KEY = "zJTqa7wwNDeDn5AT3O3SvQ";
-const GOOGLE_CLIENT_ID = "REPLACE_WITH_GOOGLE_OAUTH_CLIENT_ID"; // from Google Cloud Console, see setup instructions
+const GOOGLE_CLIENT_ID = "81404595306-7eeki2j551aeh7dmugevqasgad2vgvb1.apps.googleusercontent.com";
